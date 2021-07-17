acd7ea188f7279d4f96bfd64e78a833caaf75a2f
Pedro César <pedrocs378@gmail.com>
2021-07-17 15:50:50 -0300

    primeiro commit

---
+++ src/routes/index.ts
@@ -0,0 +1,7 @@
+import express from 'express'
+
+const routes = express.Router()
+
+routes.post('/users')
+
+export { routes }
---
+++ src/server.ts
@@ -0,0 +1,18 @@
+import 'reflect-metadata'
+import 'dotenv/config'
+import express from 'express'
+import cors from 'cors'
+
+import { routes } from './routes'
+
+import './database'
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+app.use(routes)
+
+app.listen(3333, () => {
+	console.log('Server is running...')
+})
