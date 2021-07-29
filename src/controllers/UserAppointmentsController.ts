1f9cfa8da3d04efb7bd6e03148225dfb267becf4
Pedro César <pedrocs378@gmail.com>
2021-07-29 20:25:43 -0300

    feat: retorna proximos agendamentos do usuario

--- src/controllers/UserAppointmentsController.ts
+++ src/controllers/UserAppointmentsController.ts
@@ -1,19 +1,18 @@
 import { Request, Response } from "express";
 import { getMongoRepository } from "typeorm";
-import { getDate, getMonth, getYear } from 'date-fns'
+import { isAfter } from 'date-fns'
 
 import { User } from "../database/schemas/User";
 import { Appointment } from "../database/schemas/Appointment";
 
 export class UserAppointmentsController {
 	public async index(req: Request, res: Response): Promise<Response> {
-		const { id } = req.user
-		const { day, month, year } = req.query
+		const { id: user_id } = req.user
 
 		const usersRepository = getMongoRepository(User)
 		const appointmentsRepository = getMongoRepository(Appointment)
 
-		const user = await usersRepository.findOne(id)
+		const user = await usersRepository.findOne(user_id)
 
 		if (!user) {
 			return res.status(400).json({ message: 'Usuário não existente' })
@@ -21,20 +20,14 @@ export class UserAppointmentsController {
 
 		const appointments = await appointmentsRepository.find({
 			where: {
-				user_id: user.id
+				'user.id': user.id
 			}
 		})
 
-		const appointmentsInDay = appointments.filter(appointment => {
-			const appointmentDate = new Date(appointment.date)
-
-			return (
-				getDate(appointmentDate) === Number(day) &&
-				getMonth(appointmentDate) === Number(month) &&
-				getYear(appointmentDate) === Number(year)
-			)
+		const nextAppointments = appointments.filter(appointment => {
+			return isAfter(new Date(appointment.date), Date.now())
 		})
 
-		return res.json(appointmentsInDay)
+		return res.json(nextAppointments)
 	}
 }
