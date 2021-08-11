a3149ad314e31beeab6d118ab2cf7a00ec7db39a
Pedro César <pedrocs378@gmail.com>
2021-08-11 00:22:29 -0300

    feat: adiciona erro ao tentar atualizar disponibilidade de horario em fim de semana

--- src/controllers/ProviderAvailableHoursController.ts
+++ src/controllers/ProviderAvailableHoursController.ts
@@ -24,6 +24,12 @@ export class ProviderAvailableHoursController {
 		const { provider_id } = req.params
 		const { available_hours, date } = req.body as BodyParams
 
+		const selectedDate = startOfDay(new Date(date))
+
+		if (isWeekend(selectedDate)) {
+			return res.status(400).json({ message: 'Não é possivel atualizar horários do fim de semana' })
+		}
+
 		const usersRepository = getMongoRepository(User)
 		const availabilityHoursRepository = getMongoRepository(AvailabilityHours)
 
@@ -35,7 +41,7 @@ export class ProviderAvailableHoursController {
 
 		const existingAvailableHours = await availabilityHoursRepository.findOne({
 			where: {
-				date: startOfDay(new Date(date)),
+				date: selectedDate,
 				provider_id: provider.id
 			}
 		})
@@ -50,7 +56,7 @@ export class ProviderAvailableHoursController {
 			const availabilityHour = availabilityHoursRepository.create({
 				provider_id: provider.id,
 				available_hours,
-				date: startOfDay(new Date(date))
+				date: selectedDate
 			})
 	
 			await availabilityHoursRepository.save(availabilityHour)
@@ -60,87 +66,93 @@ export class ProviderAvailableHoursController {
 	}
 
 	public async index(req: Request, res: Response): Promise<Response> {
-		const { provider_id } = req.params
-		const { day, month, year } = req.query
+		try {
+			const { provider_id } = req.params
+			const { day, month, year } = req.query
 
-		const date = {
-			day: Number(day),
-			month: Number(month),
-			year: Number(year)
-		}
-
-		const appointmentsRepository = getMongoRepository(Appointment)
-		const availableHoursRepository = getMongoRepository(AvailabilityHours)
-
-		const appointmentsData = await appointmentsRepository.find({
-			where: {
-				'provider.id': new ObjectID(provider_id)
+			const date = {
+				day: Number(day),
+				month: Number(month),
+				year: Number(year)
 			}
-		})
 
-		const appointments = classToClass(appointmentsData)
+			const appointmentsRepository = getMongoRepository(Appointment)
+			const availableHoursRepository = getMongoRepository(AvailabilityHours)
 
-		const appointmentsInDay = appointments
-			.filter(appointment => {
-				const appointmentDate = new Date(appointment.date)
-
-				return (
-					getDate(appointmentDate) === date.day &&
-					getMonth(appointmentDate) + 1 === date.month &&
-					getYear(appointmentDate) === date.year
-				)
-			})
-			.map(appointment => {
-				return {
-					...appointment,
-					provider: {
-						...appointment.provider,
-						id: appointment.provider_id
-					},
-					user: {
-						...appointment.user,
-						id: appointment.user_id
-					},
+			const appointmentsData = await appointmentsRepository.find({
+				where: {
+					'provider.id': new ObjectID(provider_id)
 				}
 			})
 
-		const existingAvailableHours = await availableHoursRepository.findOne({
-			provider_id: new ObjectID(provider_id),
-			date: new Date(date.year, date.month - 1, date.day)
-		})
+			const appointments = classToClass(appointmentsData)
 
-		const hourStart = 8
+			const appointmentsInDay = appointments
+				.filter(appointment => {
+					const appointmentDate = new Date(appointment.date)
 
-		const eachHourArray = Array.from({ length: 12 }, (_, index) => index + hourStart)
+					return (
+						getDate(appointmentDate) === date.day &&
+						getMonth(appointmentDate) + 1 === date.month &&
+						getYear(appointmentDate) === date.year
+					)
+				})
+				.map(appointment => {
+					return {
+						...appointment,
+						provider: {
+							...appointment.provider,
+							id: appointment.provider_id
+						},
+						user: {
+							...appointment.user,
+							id: appointment.user_id
+						},
+					}
+				})
 
-		const currentDate = new Date(Date.now())
-
-		const availableHoursInDay = eachHourArray.map(hour => {
-			const hasAppointmentinHour = appointmentsInDay.find(appointment => {
-				return getHours(appointment.date) === hour
+			const existingAvailableHours = await availableHoursRepository.findOne({
+				provider_id: new ObjectID(provider_id),
+				date: new Date(date.year, date.month - 1, date.day)
 			})
 
-			const isAvailableHour = existingAvailableHours 
-				? existingAvailableHours.available_hours
-					.find(existingHour => existingHour.hour === hour)?.available ?? true
-				: true
+			const hourStart = 8
 
-			const compareDate = new Date(date.year, date.month - 1, date.day, hour)
+			const eachHourArray = Array.from({ length: 12 }, (_, index) => index + hourStart)
 
-			const available = 
-				!hasAppointmentinHour && 
-				isAfter(compareDate, currentDate) &&
-				!isWeekend(compareDate) &&
-				isAvailableHour &&
-				hour !== 12
+			const currentDate = new Date(Date.now())
 
-			return {
-				hour,
-				available,
-				user: hasAppointmentinHour ? classToClass(hasAppointmentinHour.user) : null
-			}
-		})
+			const availableHoursInDay = eachHourArray.map(hour => {
+				const hasAppointmentinHour = appointmentsInDay.find(appointment => {
+					return getHours(appointment.date) === hour
+				})
 
-		return res.json(availableHoursInDay)
+				const isAvailableHour = existingAvailableHours 
+					? existingAvailableHours.available_hours
+						.find(existingHour => existingHour.hour === hour)?.available ?? true
+					: true
+
+				const compareDate = new Date(date.year, date.month - 1, date.day, hour)
+
+				const available = 
+					!hasAppointmentinHour && 
+					isAfter(compareDate, currentDate) &&
+					!isWeekend(compareDate) &&
+					isAvailableHour &&
+					hour !== 12
+
+				return {
+					hour,
+					available,
+					user: hasAppointmentinHour ? classToClass(hasAppointmentinHour.user) : null
+				}
+			})
+
+			return res.json(availableHoursInDay)
+		} catch (err) {
+			console.error(err)
+
+			return res.status(500).json(err)
+		}
 	}
 }
